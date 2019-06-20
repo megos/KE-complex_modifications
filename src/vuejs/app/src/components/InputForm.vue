b5cb762d7a9273cf1abfdbc590fbbb9e649bb796
megos <gmegos.27@gmail.com>
2019-06-20 19:37:16 +0900

    Add input form component

---
+++ src/vuejs/app/src/components/InputForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-form @submit="submit">
+    <b-input-group>
+      <b-form-input v-model="searchQuery"
+                    :disabled="disabled"
+                    :placeholder="(!disabled ? 'Search keywords...' : 'Fetching data...')">
+      </b-form-input>
+      <b-input-group-append>
+        <b-btn type="submit"
+                variant="primary">Search</b-btn>
+      </b-input-group-append>
+    </b-input-group>
+  </b-form>
+</template>
+
+<script>
+export default {
+  props: [
+    'disabled'
+  ],
+  data() {
+    return {
+      searchQuery: ''
+    }
+  },
+  methods: {
+    submit(e) {
+      e.preventDefault()
+      this.$emit('submit', this.searchQuery)
+    }
+  }
+}
+</script>
